729ae71747d00656360214024addba83cfcae0cf
Eugen Kuhn <kuhn@sendesignal.de>
2024-12-23 09:31:30 +0100

    activate eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,45 @@
-///** @type {import("eslint").Linter.Config} */
-// const config = {
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     project: "./tsconfig.json", // Verweise auf die tsconfig.json
-//   },
-//   plugins: [
-//     "@typescript-eslint",
-//   ],
-//   extends: [
-//     "next/core-web-vitals",
-//     "plugin:@typescript-eslint/recommended-type-checked",
-//     "plugin:@typescript-eslint/stylistic-type-checked",
-//   ],
-//   rules: {
-//     "@typescript-eslint/array-type": "off",
-//     "@typescript-eslint/consistent-type-definitions": "off",
-//     "@typescript-eslint/consistent-type-imports": [
-//       "warn",
-//       {
-//         "prefer": "type-imports",
-//         "fixStyle": "inline-type-imports",
-//       },
-//     ],
-//     "@typescript-eslint/no-unused-vars": [
-//       "warn",
-//       {
-//         "argsIgnorePattern": "^_",
-//       },
-//     ],
-//     "@typescript-eslint/require-await": "off",
-//     "@typescript-eslint/no-misused-promises": [
-//       "error",
-//       {
-//         "checksVoidReturn": {
-//           "attributes": false,
-//         },
-//       },
-//     ],
-//     "no-console": "warn", // Warnt bei Konsolenausgaben
-//     "@typescript-eslint/no-floating-promises": "error", // Fehler bei unbehandelten Promises
-//   },
-// };
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json", // Verweise auf die tsconfig.json
+  },
+  plugins: [
+    "@typescript-eslint",
+  ],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  rules: {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        "prefer": "type-imports",
+        "fixStyle": "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+      },
+    ],
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": {
+          "attributes": false,
+        },
+      },
+    ],
+    "no-console": "warn", // Warnt bei Konsolenausgaben
+    "@typescript-eslint/no-floating-promises": "error", // Fehler bei unbehandelten Promises
+  },
+};
 
-//module.exports = config;
+module.exports = config;
