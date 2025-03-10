51975298b43223f91162ef9d9cbee855131e5cb2
pm-dan <daniel.cipolla@pmagentur.com>
2025-03-10 20:11:39 +0100

    fix(auth): use `trustHost: true` for Coolify temporary deployments this is not recommedable because allows all connection

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -52,7 +52,7 @@ export const authConfig = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  trustHost: ["asswocgkgsoc8gsooco0okks.217.154.5.146.sslip.io"],
+  trustHost: true,
   adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
